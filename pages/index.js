2a25e15e856303157da3d5aa60f36e6ad1d0b1ac
odus-ex <at.infosec@protonmail.com>
2022-07-23 08:39:52 +0530

    Introducing RxJS for observables

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+import { fromEvent, scan } from "rxjs";
+
 export default function Home() {
+  useEffect(() => {
+    fromEvent(window.document, "click")
+      .pipe(scan((clickCount) => clickCount + 1, 0))
+      .subscribe((clickCount) => console.log(`Click count ${clickCount}`));
+  }, []);
+
   return <h1> This is home</h1>;
 }
